8041d4dbf05978f06a98fbb0130f19a394b15d5a
Aditya Baruah <154616612+mraadiscodes@users.noreply.github.com>
2023-12-22 22:37:47 +0530

    installation.sh

---
+++ installation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if running on Kali Linux
+if [ -f "/etc/debian_version" ]; then
+    # Kali Linux installation
+    sudo apt-get update
+    sudo apt-get install -y python3-pip
+elif [ -n "$(command -v termux-setup-storage)" ]; then
+    # Termux installation
+    pkg update
+    pkg install -y python
+else
+    echo "Unsupported system. Please install required dependencies manually."
+    exit 1
+fi
+
+# Install Python modules
+pip install pyfiglet 
+pip install phonenumbers
+
+echo "Installation complete. Run 'python3 phone_zone.py' to use the Phone-Zone tool."
+echo "if pyfiglet module is not installing plese run pip install pyfiglet manually"
